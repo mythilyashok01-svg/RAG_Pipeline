adcfd2ba6f3b90a91c03614b2042cfb7f8adcd5c
mythilyashok01-svg <mythilyashok01@gmail.com>
2026-06-28 11:59:44 +0530

    Add files via upload

---
+++ python_basics.py
@@ -0,0 +1,23 @@
+print("hello world")
+name="Mythili"
+age=27
+married=False
+salary=4.5 
+print(type(name))
+print(type(age))
+print(type(married))
+print(type(salary))
+f_name=input("Enter your name: ")
+print ("Welcome",f_name,name, age,"married" ,married,salary)
+print(f"welcome {f_name} Full name is : {name} the age {age} is married {married} and the salary is {salary}")#f-strings
+#This is my first program - single line comment 
+'''multi line comment 
+working 
+'''
+
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) #always returns float
